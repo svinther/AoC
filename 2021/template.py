06ea7e3e436fef07f82c6b3e42a8e84c4d711315
Steffen Vinther Sørensen <svso@stibosystems.com>
2022-11-29 06:40:24 +0100

    template

---
+++ 2021/template.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+
+DAY = 16
+YEAR = 2021
+full_input_ = Path(f"input.{YEAR}.{DAY}.txt").read_text()
+
+
+def solve(parsed):
+    pass
+
+
+def parse(input: str):
+    pass
+
+
+if __name__ == "__main__":
+    parsed = parse(full_input_)
+    result = solve(parsed)
+    print(result)
