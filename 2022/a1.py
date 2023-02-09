d996170c05b34736c6eff993b7298af2ca01d287
Steffen Vinther Sørensen <svso@stibosystems.com>
2023-02-09 08:34:05 +0100

    minimize

--- 2022/a1.py
+++ 2022/a1.py
@@ -3,31 +3,6 @@ from pathlib import Path
 DAY = 1
 full_input_ = Path(f"{DAY}.txt").read_text()
 
-
-def solve(parsed):
-    sums = [sum(e) for e in parsed]
-    sums.sort()
-    return max(sums), sum(sums[-3:])
-
-
-def parse(input: str):
-    A = []
-    current = []
-    for l in input.split("\n"):
-        l = l.strip()
-        if not l:
-            if current:
-                A.append(current)
-                current = []
-            continue
-        cals = int(l)
-        current.append(cals)
-    if current:
-        A.append(current)
-    return A
-
-
-if __name__ == "__main__":
-    parsed = parse(full_input_)
-    result = solve(parsed)
-    print(result)
+E = [sum(map(int, chunk.split("\n"))) for chunk in full_input_.split("\n\n")]
+print(max(E))
+print(sum(sorted(E)[-3:]))
