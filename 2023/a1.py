b68d18e90d409f10ae9da8af1a3d1a6307e456de
Steffen Vinther Sørensen <svso@stibosystems.com>
2023-12-02 07:51:46 +0100

    black

--- 2023/a1.py
+++ 2023/a1.py
@@ -28,17 +28,17 @@ def parse(input_: str):
         if not l:
             continue
         digits = []
-        #one, two, three, four, five, six, seven
-        spelled={
-            "one":1,
-            "two":2,
-            "three":3,
-            "four":4,
-            "five":5,
-            "six":6,
-            "seven":7,
-            "eight":8,
-            "nine":9,
+        # one, two, three, four, five, six, seven
+        spelled = {
+            "one": 1,
+            "two": 2,
+            "three": 3,
+            "four": 4,
+            "five": 5,
+            "six": 6,
+            "seven": 7,
+            "eight": 8,
+            "nine": 9,
         }
         for i in range(len(l)):
             if l[i].isdigit():
