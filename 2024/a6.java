f4a6da28129bc06a58386b7aa8fd1caa651a0336
Steffen Vinther Sørensen <svso@stibosystems.com>
2024-12-08 17:29:10 +0100

    optimized to 400ms

--- 2024/a6.java
+++ 2024/a6.java
@@ -2,17 +2,14 @@
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 
 public class a6 {
 
-    public static Set<Position> numsteps(char[][] G, Position block) {
+    private static Set<Position> calculatePositions(char[][] G, Position block) {
         int R = G.length;
         int C = G[0].length;
 
@@ -20,10 +17,12 @@ public class a6 {
         int[][] dirs = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
         int d = 0;
 
+        l1:
         for (int r = 0; r < R; r++) {
             for (int c = 0; c < C; c++) {
                 if (G[r][c] == '^') {
                     state = new State(r, c, d);
+                    break l1;
                 }
             }
         }
@@ -33,28 +32,27 @@ public class a6 {
         Set<Position> positions = new HashSet();
 
         do {
-            if (state.r < 0 || state.r >= R || state.c < 0 || state.c >= C) {
+            if (state.r() < 0 || state.r() >= R || state.c() < 0 || state.c() >= C) {
                 return positions;
             }
 
-            if (SEEN.contains(state)) {
+            if (!SEEN.add(state)) {
                 return null;
             }
-            SEEN.add(state);
 
-            positions.add(new Position(state.r, state.c));
+            positions.add(new Position(state.r(), state.c()));
 
             while (true) {
-                int nrow = state.r + dirs[d % 4][0];
-                int ncol = state.c + dirs[d % 4][1];
+                int nrow = state.r() + dirs[d % 4][0];
+                int ncol = state.c() + dirs[d % 4][1];
                 if (nrow >= 0 && nrow < R && ncol >= 0 && ncol < C) {
-                    if (G[nrow][ncol] == '#' || (block != null && block.r == nrow && block.c == ncol)) {
+                    if (G[nrow][ncol] == '#' || (block != null && block.r() == nrow && block.c() == ncol)) {
                         d++;
                         continue;
                     }
                 }
 
-                state = new State(nrow, ncol, d%4);
+                state = new State(nrow, ncol, d % 4);
                 break;
             }
 
@@ -70,107 +68,24 @@ public class a6 {
             G[r] = Gs.get(r).toCharArray();
         }
 
-        Set<Position> guardPositions = numsteps(G, null);
-        System.out.println(guardPositions.size());
+        Set<Position> guardPositions = calculatePositions(G, null);
+        int p1 = guardPositions.size();
+        System.out.println(p1);
 
-        ExecutorService executorService = Executors.newFixedThreadPool(4); // 4 threads
-        List<Future<Set<Position>>> futures = new ArrayList();
-        for (Position gp : guardPositions) {
-            if (G[gp.r][gp.c] == '.') {
-                futures.add(executorService.submit(() -> numsteps(G, new Position(gp.r, gp.c))));
-            }
-        }
+        long p2 = guardPositions.parallelStream()
+                .filter(gp -> G[gp.r()][gp.c()] == '.')
+                .map(gp -> calculatePositions(G, new Position(gp.r(), gp.c())))
+                .filter(Objects::isNull)
+                .count();
 
-        int p2 = 0;
-        for (Future<Set<Position>> future : futures) {
-            if (future.get() == null)
-                p2++;
-        }
-        executorService.shutdownNow();
-        
         System.out.println(p2);
-
     }
 }
 
-class Position {
-
-    int r;
-    int c;
-
-    public Position(int r, int c) {
-        this.c = c;
-        this.r = r;
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 7;
-        hash = 13 * hash + this.r;
-        hash = 13 * hash + this.c;
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final Position other = (Position) obj;
-        if (this.r != other.r) {
-            return false;
-        }
-        return this.c == other.c;
-    }
+record Position(int r, int c) {
 
 }
 
-class State {
-
-    int r;
-    int c;
-    int d;
-
-    public State(int r, int c, int d) {
-        this.c = c;
-        this.d = d;
-        this.r = r;
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 7;
-        hash = 29 * hash + this.r;
-        hash = 29 * hash + this.c;
-        hash = 29 * hash + this.d;
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final State other = (State) obj;
-        if (this.r != other.r) {
-            return false;
-        }
-        if (this.c != other.c) {
-            return false;
-        }
-        return this.d == other.d;
-    }
+record State(int r, int c, int d) {
 
 }
