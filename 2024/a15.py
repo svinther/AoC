fa391df061e8b6825fa8c5bb827a872daa0dea7e
Steffen Vinther Sørensen <svso@stibosystems.com>
2024-12-15 10:25:59 +0100

    2024 15

--- 2024/a15.py
+++ 2024/a15.py
@@ -3,54 +3,6 @@ from heapq import *
 from collections import *
 
 
-def render(G):
-    res = []
-    for row in G:
-        res.append("".join(row))
-    return "\n".join(res)
-
-
-def solvep1(parsed):
-    G, moves = parsed
-    R = len(G)
-    C = len(G[0])
-    D = {"<": (0, -1), "^": (-1, 0), "v": (1, 0), ">": (0, 1)}
-
-    for r in range(R):
-        for c in range(C):
-            if G[r][c] == "@":
-                rob = (r, c)
-
-    def push(p, d):
-        r, c = p
-        dr, dc = d
-        nbr, nbc = r + dr, c + dc
-        if 0 <= nbr < R and 0 <= nbc < C:
-            if G[nbr][nbc] == "#":
-                return False
-
-            if G[nbr][nbc] == "." or push((nbr, nbc), d):
-                G[nbr][nbc] = G[r][c]
-                G[r][c] = "."
-                return True
-        return False
-
-    for m in moves:
-        d = D[m]
-        if push(rob, d):
-            robr, robc = rob
-            dr, dc = d
-            rob = robr + dr, robc + dc
-
-    answer = 0
-    for r in range(R):
-        for c in range(C):
-            if G[r][c] == "O":
-                answer += 100 * r + c
-
-    return answer
-
-
 def expand(G):
     G_ = []
     for row in G:
@@ -68,9 +20,10 @@ def expand(G):
     return G_
 
 
-def solvep2(parsed):
+def solve(parsed, p2=False):
     G, moves = parsed
-    G = expand(G)
+    if p2:
+        G = expand(G)
 
     R = len(G)
     C = len(G[0])
@@ -87,7 +40,7 @@ def solvep2(parsed):
 
         nbp = set()
         for r, c in p:
-            assert G[r][c] in ("[", "]", "@"), f"Unmovable object {G[r][c]}"
+            assert G[r][c] in ("[", "]", "@", "O"), f"Unmovable object {G[r][c]}"
 
             nbr, nbc = r + dr, c + dc
             if 0 > nbr or nbr >= R or 0 > nbc or nbc >= C:
@@ -108,7 +61,7 @@ def solvep2(parsed):
 
         if len(nbp) == 0 or push(nbp, d):
             for r, c in p:
-                assert G[r][c] in ("[", "]", "@"), f"Unmovable object {G[r][c]}"
+                assert G[r][c] in ("[", "]", "@", "O"), f"Unmovable object {G[r][c]}"
 
                 nbr, nbc = r + dr, c + dc
                 assert G[nbr][nbc] == ".", f"moving to non dot {G[nbr][nbc]}"
@@ -123,11 +76,13 @@ def solvep2(parsed):
         answer = 0
         for r in range(R):
             for c in range(C):
-                if G[r][c] == "[":
-                    assert G[r][c + 1] == "]"
+                if G[r][c] in ("O", "["):
                     answer += 100 * r + c
+
                 if G[r][c] == "]":
                     assert G[r][c - 1] == "["
+                if G[r][c] == "[":
+                    assert G[r][c + 1] == "]"
         return answer
 
     for m in moves:
@@ -175,7 +130,7 @@ def testp1p2():
 <^^>>>vv<v>>v<<
 """
     parsed = parse(input_)
-    assert solvep1(parsed) == 2028
+    assert solve(parsed) == 2028
 
 
 def testlarger():
@@ -203,9 +158,9 @@ vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v
 v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^    
 """
     parsed = parse(input_)
-    assert solvep1(parsed) == 10092
+    assert solve(parsed) == 10092
     parsed = parse(input_)
-    assert solvep2(parsed) == 9021
+    assert solve(parsed, p2=True) == 9021
 
 
 def testp2ex():
@@ -221,7 +176,7 @@ def testp2ex():
 <vv<<^^<<^^
 """
     parsed = parse(input_)
-    assert solvep2(parsed) == 618
+    assert solve(parsed, p2=True) == 618
 
 
 def testmini():
@@ -234,17 +189,17 @@ def testmini():
 >>><<v><v>>>>^
 """
     parsed = parse(input_)
-    solvep2(parsed)
+    solve(parsed, p2=True)
 
 
 def run():
     input_ = open(0).read()
     parsed = parse(input_)
-    p1result = solvep1(parsed)
+    p1result = solve(parsed)
     print(p1result)
 
     parsed = parse(input_)
-    p2result = solvep2(parsed)
+    p2result = solve(parsed, p2=True)
     print(p2result)
 
 
